318c439f496c334edc8685454d39c2b1c8ad250f
jadeofwar <devore.jay@gmail.com>
2025-12-19 22:04:42 -0500

    Correctly map Airtable Table IDs: Pricing (tbliVrYxBgWFJ3AF1) and FAQ (tblkfsPmorGvt1o2q)

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ document.querySelectorAll('a[href^="sms:"]').forEach(link => {
 const AIRTABLE_CONFIG = {
     apiKey: 'pat' + 'BCrI4nLXMrGfWc.92e4c7bb7593052d499ae550bc989568b5831a1610db788b20f182b7a5f0c31e',
     baseId: 'appYB5fLt8X3Q117v',
-    pricingTable: 'tblkfsPmorGvt1o2q', // Using Table ID for robustness
-    faqTable: 'FAQ Table'
+    pricingTable: 'tbliVrYxBgWFJ3AF1',
+    faqTable: 'tblkfsPmorGvt1o2q'
 };
 
 // Store pricing data globally for floor plan tabs
